07550f15f5589c4f1f21e5a5adeed38223d717dc
Kira-Vishn9 <Kvishn9@gmail.com>
2023-09-22 13:52:34 -0400

    feat: fix main for vps

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {AppModule} from './module/app/app.module'
 async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://146.59.16.17:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   })
@@ -12,4 +12,3 @@ async function bootstrap (): Promise<void> {
 }
 
 bootstrap()
-// http://146.59.16.17:5173
